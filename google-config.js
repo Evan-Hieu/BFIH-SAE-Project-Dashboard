90600e675a48977b6ad121bccebad8900b4b629a
Hieu Duong Minh <hieusword@gmail.com>
2026-08-30 16:50:55 +0530

    Configure Google OAuth client for private SAE sync

--- google-config.js
+++ google-config.js
@@ -1,2 +1,2 @@
 // Public OAuth client ID only. NEVER put a client secret or access token here.
-window.SAE_GOOGLE_CLIENT_ID = '';
+window.SAE_GOOGLE_CLIENT_ID = '492543731435-6022j8hevc78905inrvcpcfein03rp5r.apps.googleusercontent.com';
